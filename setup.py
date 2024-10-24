a40e3dbab90649b94f9e4cc1ded677d981df0fca
Advait Pathak <146539696+Advait-Pathak9222@users.noreply.github.com>
2024-10-24 19:34:28 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A small cchrome plugin to detect youtube comment sentiments',
+    author='A small chrome plugin to detect youtube comment sentiments',
+    license='',
+)
